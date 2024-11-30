f3d9beb1d047616fa645d9f2e2f1558331855fbe
Deepak Malik <deepak16031@iiitd.ac.in>
2024-11-30 23:02:19 +0530

    arrays and slices

--- iteration/sum_test.go
+++ iteration/sum_test.go
@@ -1,6 +1,9 @@
 package iteration
 
-import "testing"
+import (
+	"reflect"
+	"testing"
+)
 
 func TestSum(t *testing.T) {
 	t.Run("Collection of 5 numbers", func(t *testing.T) {
@@ -13,4 +16,34 @@ func TestSum(t *testing.T) {
 		}
 	})
 
+}
+
+func TestSumAll(t *testing.T) {
+	actual := SumAll([]int{1, 2}, []int{0, 9})
+	expected := []int{3, 9}
+
+	if !reflect.DeepEqual(actual, expected) {
+		t.Errorf("expected %v actual %v", expected, actual)
+	}
+}
+
+func TestSumAllTails(t *testing.T) {
+
+	checkSums := func(t testing.TB, actual, expected []int) {
+		if !reflect.DeepEqual(actual, expected) {
+			t.Errorf("expected %v actual %v", expected, actual)
+		}
+	}
+
+	t.Run("sum the tails of slices", func(t *testing.T) {
+		actual := SumAllTails([]int{1, 2}, []int{0, 9})
+		expected := []int{2, 9}
+		checkSums(t, actual, expected)
+	})
+
+	t.Run("sum tails of empty slices safely", func(t *testing.T) {
+		actual := SumAllTails([]int{}, []int{})
+		expected := []int{0, 0}
+		checkSums(t, actual, expected)
+	})
 }
